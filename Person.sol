1e61d71c1970e3bf7c14c7dca447da5ccdea5096
Seth Powelson <sethmadspowelson@gmail.com>
2020-10-28 16:38:41 -0600

    error handling with require()

--- Person.sol
+++ Person.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.12;
+pragma solidity 0.5.12;
 
 contract HelloWorld {
     
@@ -9,11 +9,21 @@ contract HelloWorld {
         bool isSenior;
     }
     
+    address public owner;
+    
+    // runs whenever contract is created
+    constructor() public {
+        owner = msg.sender;
+    }
+    
     // key value pair; address is key (data type) we search for to return the person (value)
     // people is name of mapping
     mapping(address => Person) private people;
     
+    address[] private creators;
+    
     function createPerson(string memory name, uint age, uint height) public {
+        require(age < 150, "Age needs to be below 150");
         //this creates a Person
         Person memory newPerson;
         newPerson.name = name;
@@ -25,6 +35,7 @@ contract HelloWorld {
             newPerson.isSenior = false;
         }
         insertPerson(newPerson);
+        creators.push(msg.sender);
         
     }
     
@@ -36,9 +47,21 @@ contract HelloWorld {
         people[creator] = newPerson;
     }
     
+    // view means its a get function (doesnt modify contract in any way, just returns a variable)
     function getPerson() public view returns(string memory name, uint age, uint height, bool isSenior) {
         address creator = msg.sender;
         return (people[creator].name, people[creator].age, people[creator].height, people[creator].isSenior);
+    }
+    
+    function deletePerson(address creator) public {
+        require(msg.sender == owner);
+        delete people[creator];
+    }
+    
+    // view means its a get function (doesnt modify contract in any way, just returns a variable)
+    function getCreator(uint index) public view returns(address) {
+        require(msg.sender == owner, "Caller must be the owner.");
+        return creators[index];
     }
     
 }
